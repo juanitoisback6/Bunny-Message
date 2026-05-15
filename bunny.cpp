3a93d4eda456a9d7cba279b8f1e2153393583b82
Dakitidunno <juanchojuan6@hotmail.com>
2026-05-14 19:28:36 -0500

    first commit

---
+++ bunny.cpp
@@ -0,0 +1,15 @@
+ 
+
+#include <iostream> 
+
+int main() {
+  std::cout << "|￣￣￣￣￣￣￣|\n";
+  std::cout << "| 14/05/2026 |\n"; 
+  std::cout << "|    Hello   |\n"; 
+  std::cout << "|____________|\n"; 
+  std::cout << "(\__/) ||\n"; 
+  std::cout << "(•ㅅ•) ||\n"; 
+  std::cout << "/ 　 づ\n";  
+}
+  
+ 
