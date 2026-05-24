e6a742445d1ad3afa74c6a978286ff0d8541b777
Dakitidunno <207196558+juanitoisback6@users.noreply.github.com>
2026-05-23 23:35:42 -0500

    Update bunny.cpp

--- bunny.cpp
+++ bunny.cpp
@@ -1,13 +1,14 @@
  
-
+#include <windows.h> 
 #include <iostream> 
 
-int main() {
+int main() {  
+  SetConsoleOutputCP(CP_UTF8);
   std::cout << "|￣￣￣￣￣￣￣|\n";
   std::cout << "| 14/05/2026 |\n"; 
   std::cout << "|    Hello   |\n"; 
   std::cout << "|____________|\n"; 
-  std::cout << "(\__/) ||\n"; 
+  std::cout << "(\\__/) ||\n"; 
   std::cout << "(•ㅅ•) ||\n"; 
   std::cout << "/ 　 づ\n";  
 }
